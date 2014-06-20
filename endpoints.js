62e5c72f565620b9f4c2bcaeaf96cf7dc330ca44
zubairov <renat.zubairov@elastic.io>
2014-06-20 10:51:47 +0200

    Implemented antiscan middleware

---
+++ endpoints.js
@@ -0,0 +1,8 @@
+module.exports = function() {
+    return [
+        "/public/js/fckeditor/editor/filemanager/upload/test.html",
+        "/xmlrpc.php",
+        "/wp-login.php",
+        "/administrator/index.php"
+    ]
+};
---
+++ index.js
@@ -0,0 +1,56 @@
+/*!
+ * express-antiscan
+ * Copyright(c) 2014 elastic.io GmbH
+ *
+ * MIT Licensed
+ */
+var debug = require('debug')('express-antiscan');
+
+var DEFAULT_TIMEOUT = 25000; // Default timeout in milliseconds
+
+exports = module.exports = function(options) {
+    options = extend({
+        timeout : DEFAULT_TIMEOUT
+    }, options);
+    var endpoints = require('./endpoints')();
+    endpoints = endpoints.concat(options.endpoints || []);
+    debug('Found %s endpoints, [%s]', endpoints.length, endpoints.join(','));
+
+    var timeout = options.timeout;
+    debug('Configured timeout %s ms', timeout);
+
+    return function handleScan(req,res,next) {
+        var path = req.path, found;
+        endpoints.forEach(function(endpoint) {
+            if (endpoint === req.path) {
+                found = true;
+            }
+        });
+        if (found) {
+            setTimeout(function() {
+                res.send(404, 'Not found');
+            }, timeout);
+        } else {
+            return next();
+        }
+    }
+};
+
+/**
+ * Shallow clone a single object.
+ *
+ * @param {Object} obj
+ * @param {Object} source
+ * @return {Object}
+ * @api private
+ */
+
+function extend(obj, source) {
+    if (!source) return obj;
+
+    for (var prop in source) {
+        obj[prop] = source[prop];
+    }
+
+    return obj;
+};
